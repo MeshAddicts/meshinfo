7f520cc90c9cf4404c35096d8011852a8d6d3b7f
Kevin Elliott <kevin@welikeinc.com>
2024-05-30 03:13:25 -0700

    add local build script

---
+++ build-push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# set version from args
+
+# build and push
+
+# set version from args if not, exit
+if [ -z "$1" ]
+  then
+    echo "No version supplied"
+    exit 1
+fi
+
+REPO=kevinelliott/meshinfo
+VERSION=$1
+
+docker build -t ghcr.io/$REPO:$VERSION --platform=linux/amd64 . && docker push ghcr.io/$REPO:$VERSION
