a5cbbfdc00400978de390deec3fe83d73fcbdbae
Kevin Elliott <kevin@welikeinc.com>
2024-06-28 20:02:41 -0700

    skeleton for fastapi implementation

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -5,8 +6,15 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="./templates/api")
 app = FastAPI()
 
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html.j2", context={}
-    )
+    return templates.TemplateResponse(request=request, name="index.html.j2", context={})
+
+
+async def start_uvicorn(loop):
+    config = uvicorn.Config(app, host="0.0.0.0", port=9000, loop=loop)
+    server = uvicorn.Server(config)
+    print(f"Starting Uvicorn server bound at http://{config.host}:{config.port}")
+    await server.serve()
+    print("Uvicorn server stopped")
