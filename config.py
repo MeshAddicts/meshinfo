88de5df1203931a1425a6fb751673bf52873a3cc
Kevin Elliott <kevin@welikeinc.com>
2024-06-30 16:45:53 -0700

    Make the MQTT client id more dynamic

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 
 import datetime
 import json
+import uuid
 
 class Config:
   @classmethod
   def load(cls):
     config = cls.load_from_file('config.json')
+    random_uuid = str(uuid.uuid4())
+    config['broker']['client_id'] = config['broker']['client_id_prefix'] + '-' + random_uuid
     config['server']['start_time'] = datetime.datetime.now(datetime.timezone.utc).astimezone()
     return config
 
