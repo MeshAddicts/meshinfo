28b27e51b84cc2fb48c3f3c14a1ad6e1420a1e79
Kevin Elliott <kevin@welikeinc.com>
2024-07-11 23:31:18 -0700

    add version info to the layout

--- config.py
+++ config.py
@@ -10,6 +10,14 @@ class Config:
     random_uuid = str(uuid.uuid4())
     config['broker']['client_id'] = config['broker']['client_id_prefix'] + '-' + random_uuid
     config['server']['start_time'] = datetime.datetime.now(datetime.timezone.utc).astimezone()
+
+    try:
+      version_info = cls.load_from_file('version-info.json')
+      if version_info is not None:
+        config['server']['version_info'] = version_info
+    except FileNotFoundError:
+      pass
+
     return config
 
   @classmethod
