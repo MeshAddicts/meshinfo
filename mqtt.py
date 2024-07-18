c9a2370d51ebb7fe37f844849cc3882921739c07
Kevin Elliott <kevin@welikeinc.com>
2024-07-18 02:48:03 -0700

    mqtt protobuf bug fix for traceroutes

--- mqtt.py
+++ mqtt.py
@@ -70,177 +70,179 @@ class MQTT:
                 await asyncio.sleep(5)
 
     async def process_mqtt_msg(self, client, msg):
-        if '/2/e/' in msg.topic.value or '/2/map/' in msg.topic.value:
-            if self.config['debug']:
-                print(f"Received a protobuf message: {msg.topic} {msg.payload}")
-            is_encrypted = False
-            mp = mesh_pb2.MeshPacket()
-            outs = {}
-
-            try:
-                se = mqtt_pb2.ServiceEnvelope()
-                se.ParseFromString(msg.payload)
-                mp = se.packet
-                outs = json.loads(MessageToJson(mp, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+        if self.config['broker']['decoders']['protobuf']['enabled']:
+            if'/2/e/' in msg.topic.value or '/2/map/' in msg.topic.value:
                 if self.config['debug']:
-                    print(f"Decoded protobuf message: {outs}")
-            except Exception as _:
-                # print(f"*** ParseFromString: {str(e)}")
-                pass
+                    print(f"Received a protobuf message: {msg.topic} {msg.payload}")
+                is_encrypted = False
+                mp = mesh_pb2.MeshPacket()
+                outs = {}
 
-            if mp.HasField("encrypted") and not mp.HasField("decoded"):
-                is_encrypted = True
-                for key_item in self.config['broker']['channels']['encryption']:
-                    key_bytes = base64.b64decode(key_item['key'].encode('ascii'))
-                    try:
-                        if self.config['debug']:
-                            print(f"Attempting decryption with key: {key}")
-                        nonce_packet_id = getattr(mp, "id").to_bytes(8, "little")
-                        nonce_from_node = getattr(mp, "from").to_bytes(8, "little")
-                        nonce = nonce_packet_id + nonce_from_node
-                        cipher = Cipher(algorithms.AES(key_bytes), modes.CTR(nonce), backend=default_backend())
-                        decryptor = cipher.decryptor()
-                        decrypted_bytes = decryptor.update(getattr(mp, "encrypted")) + decryptor.finalize()
-                        data = mesh_pb2.Data()
-                        data.ParseFromString(decrypted_bytes)
-                        mp.decoded.CopyFrom(data)
-                        outs = json.loads(MessageToJson(mp, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
-                        break
-                    except Exception as e:
-                        if self.config['debug']:
-                            print(f"*** Decryption failed: {str(e)}")
-                        continue
+                try:
+                    se = mqtt_pb2.ServiceEnvelope()
+                    se.ParseFromString(msg.payload)
+                    mp = se.packet
+                    outs = json.loads(MessageToJson(mp, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: {outs}")
+                except Exception as _:
+                    # print(f"*** ParseFromString: {str(e)}")
+                    pass
 
-            outs['rssi'] = mp.rx_rssi
-            outs['snr'] = mp.rx_snr
-            outs['timestamp'] = mp.rx_time
+                if mp.HasField("encrypted") and not mp.HasField("decoded"):
+                    is_encrypted = True
+                    for key_item in self.config['broker']['channels']['encryption']:
+                        key_bytes = base64.b64decode(key_item['key'].encode('ascii'))
+                        try:
+                            if self.config['debug']:
+                                print(f"Attempting decryption with key: {key}")
+                            nonce_packet_id = getattr(mp, "id").to_bytes(8, "little")
+                            nonce_from_node = getattr(mp, "from").to_bytes(8, "little")
+                            nonce = nonce_packet_id + nonce_from_node
+                            cipher = Cipher(algorithms.AES(key_bytes), modes.CTR(nonce), backend=default_backend())
+                            decryptor = cipher.decryptor()
+                            decrypted_bytes = decryptor.update(getattr(mp, "encrypted")) + decryptor.finalize()
+                            data = mesh_pb2.Data()
+                            data.ParseFromString(decrypted_bytes)
+                            mp.decoded.CopyFrom(data)
+                            outs = json.loads(MessageToJson(mp, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+                            break
+                        except Exception as e:
+                            if self.config['debug']:
+                                print(f"*** Decryption failed: {str(e)}")
+                            continue
 
-            # TODO: Need to handle this
-            # self.handle_log(mp.decoded)
+                outs['rssi'] = mp.rx_rssi
+                outs['snr'] = mp.rx_snr
+                outs['timestamp'] = mp.rx_time
 
-            if mp.decoded.portnum == portnums_pb2.TEXT_MESSAGE_APP:
-                text = mp.decoded.payload.decode("utf-8")
-                payload = { "text": text }
-                outs["type"] = "text"
-                outs["payload"] = payload
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: text: {outs}")
-                await self.handle_text(outs)
+                # TODO: Need to handle this
+                # self.handle_log(mp.decoded)
 
-            elif mp.decoded.portnum == portnums_pb2.MAP_REPORT_APP:
-                report = mesh_pb2.Position().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(report, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True, always_print_fields_with_no_presence=True))
-                outs["type"] = "mapreport"
-                outs["payload"] = out
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: mapreport: {outs}")
-                # self.handle_mapreport(outs)
+                if mp.decoded.portnum == portnums_pb2.TEXT_MESSAGE_APP:
+                    text = mp.decoded.payload.decode("utf-8")
+                    payload = { "text": text }
+                    outs["type"] = "text"
+                    outs["payload"] = payload
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: text: {outs}")
+                    await self.handle_text(outs)
 
-            elif mp.decoded.portnum == portnums_pb2.NEIGHBORINFO_APP:
-                info = mesh_pb2.NeighborInfo().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(info, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True, always_print_fields_with_no_presence=True))
-                outs["type"] = "neighborinfo"
-                outs["payload"] = out
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: neighborinfo: {outs}")
-                await self.handle_neighborinfo(outs)
+                elif mp.decoded.portnum == portnums_pb2.MAP_REPORT_APP:
+                    report = mesh_pb2.Position().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(report, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True, always_print_fields_with_no_presence=True))
+                    outs["type"] = "mapreport"
+                    outs["payload"] = out
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: mapreport: {outs}")
+                    # self.handle_mapreport(outs)
 
-            elif mp.decoded.portnum == portnums_pb2.NODEINFO_APP:
-                info = mesh_pb2.User().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(info, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
-                if isinstance(out['id'], int):
-                    out["id"] = utils.convert_node_id_from_int_to_hex(out['id'])
-                out["id"] = out['id'].replace('!', '')
-                outs["type"] = "nodeinfo"
-                outs["payload"] = out
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: nodeinfo: {outs}")
-                await self.handle_nodeinfo(outs)
+                elif mp.decoded.portnum == portnums_pb2.NEIGHBORINFO_APP:
+                    info = mesh_pb2.NeighborInfo().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(info, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True, always_print_fields_with_no_presence=True))
+                    outs["type"] = "neighborinfo"
+                    outs["payload"] = out
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: neighborinfo: {outs}")
+                    await self.handle_neighborinfo(outs)
 
-            elif mp.decoded.portnum == portnums_pb2.ROUTING_APP:
-                data = mesh_pb2.Routing().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(data, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
-                outs["type"] = "routing"
-                outs["payload"] = out
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: routing: {outs}")
-                # self.handle_routing(outs)
+                elif mp.decoded.portnum == portnums_pb2.NODEINFO_APP:
+                    info = mesh_pb2.User().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(info, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+                    if isinstance(out['id'], int):
+                        out["id"] = utils.convert_node_id_from_int_to_hex(out['id'])
+                    out["id"] = out['id'].replace('!', '')
+                    outs["type"] = "nodeinfo"
+                    outs["payload"] = out
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: nodeinfo: {outs}")
+                    await self.handle_nodeinfo(outs)
 
-            elif mp.decoded.portnum == portnums_pb2.TRACEROUTE_APP:
-                route = mesh_pb2.RouteDiscovery().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(route, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True, always_print_fields_with_no_presence=True))
-                if 'route' in out:
-                    route = []
-                    for r in out['route']:
-                        id = utils.convert_node_id_from_int_to_hex(r)
-                        route.append(id)
-                    outs["route"] = route
-                outs["type"] = "traceroute"
-                outs["payload"] = out
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: traceroute: {outs}")
-                await self.handle_traceroute(outs)
+                elif mp.decoded.portnum == portnums_pb2.ROUTING_APP:
+                    data = mesh_pb2.Routing().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(data, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+                    outs["type"] = "routing"
+                    outs["payload"] = out
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: routing: {outs}")
+                    # self.handle_routing(outs)
 
-            elif mp.decoded.portnum == portnums_pb2.POSITION_APP:
-                pos = mesh_pb2.Position().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(pos, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
-                outs["type"] = "position"
-                outs["payload"] = out
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: position: {outs}")
-                await self.handle_position(outs)
-
-            elif mp.decoded.portnum == portnums_pb2.TELEMETRY_APP:
-                env = telemetry_pb2.Telemetry().FromString(mp.decoded.payload)
-                out = json.loads(MessageToJson(env, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
-                if 'rx_time' in outs:
-                    out['timestamp'] = datetime.datetime.fromtimestamp(outs['rx_time'] / 1000).astimezone(ZoneInfo(self.config['server']['timezone']))
-                outs["type"] = "telemetry"
-                if 'device_metrics' in out:
-                    outs["payload"] = out['device_metrics']
-                if 'environment_metrics' in out:
-                    outs["payload"] = out['environment_metrics']
-                if self.config['debug']:
-                    print(f"Decoded protobuf message: telemetry: {outs}")
-                await self.handle_telemetry(outs)
-
-            else:
-                if self.config['debug']:
-                    print(f"Received an unknown protobuf message: {mp}")
-
-        elif '/2/json/' in msg.topic.value:
-            if self.config['debug']:
-                print(f"Received a JSON message: {msg.topic} {msg.payload}")
-            try:
-                decoded = msg.payload.decode("utf-8")
-                j = json.loads(decoded, cls=_JSONDecoder)
-                await self.handle_log(msg)
-                if j['type'] == "neighborinfo":
-                    await self.handle_neighborinfo(j)
-                if j['type'] == "nodeinfo":
-                    await self.handle_nodeinfo(j)
-                if j['type'] == "position":
-                    await self.handle_position(j)
-                if j['type'] == "telemetry":
-                    await self.handle_telemetry(j)
-                if j['type'] == "text":
-                    await self.handle_text(j)
-                if j['type'] == "traceroute":
-                    if 'route' in j['payload']:
+                elif mp.decoded.portnum == portnums_pb2.TRACEROUTE_APP:
+                    route = mesh_pb2.RouteDiscovery().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(route, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True, always_print_fields_with_no_presence=True))
+                    if 'route' in out:
                         route = []
-                        for r in j['payload']['route']:
-                            node = self.data.find_node_by_longname(r)
-                            if node is not None:
-                                id = node['id']
-                            else:
-                                id = None
+                        for r in out['route']:
+                            id = utils.convert_node_id_from_int_to_hex(int(r))
                             route.append(id)
-                        j['route'] = route
-                    await self.handle_traceroute(j)
-                await self.prune_expired_nodes()
-            except Exception as e:
-                print(e)
-                traceback.print_exc()
+                        outs["route"] = route
+                    outs["type"] = "traceroute"
+                    outs["payload"] = out
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: traceroute: {outs}")
+                    await self.handle_traceroute(outs)
+
+                elif mp.decoded.portnum == portnums_pb2.POSITION_APP:
+                    pos = mesh_pb2.Position().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(pos, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+                    outs["type"] = "position"
+                    outs["payload"] = out
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: position: {outs}")
+                    await self.handle_position(outs)
+
+                elif mp.decoded.portnum == portnums_pb2.TELEMETRY_APP:
+                    env = telemetry_pb2.Telemetry().FromString(mp.decoded.payload)
+                    out = json.loads(MessageToJson(env, preserving_proto_field_name=True, ensure_ascii=False, indent=2, sort_keys=True, use_integers_for_enums=True))
+                    if 'rx_time' in outs:
+                        out['timestamp'] = datetime.datetime.fromtimestamp(outs['rx_time'] / 1000).astimezone(ZoneInfo(self.config['server']['timezone']))
+                    outs["type"] = "telemetry"
+                    if 'device_metrics' in out:
+                        outs["payload"] = out['device_metrics']
+                    if 'environment_metrics' in out:
+                        outs["payload"] = out['environment_metrics']
+                    if self.config['debug']:
+                        print(f"Decoded protobuf message: telemetry: {outs}")
+                    await self.handle_telemetry(outs)
+
+                else:
+                    if self.config['debug']:
+                        print(f"Received an unknown protobuf message: {mp}")
+
+        elif self.config['broker']['decoders']['json']['enabled']:
+            if '/2/json/' in msg.topic.value:
+                if self.config['debug']:
+                    print(f"Received a JSON message: {msg.topic} {msg.payload}")
+                try:
+                    decoded = msg.payload.decode("utf-8")
+                    j = json.loads(decoded, cls=_JSONDecoder)
+                    await self.handle_log(msg)
+                    if j['type'] == "neighborinfo":
+                        await self.handle_neighborinfo(j)
+                    if j['type'] == "nodeinfo":
+                        await self.handle_nodeinfo(j)
+                    if j['type'] == "position":
+                        await self.handle_position(j)
+                    if j['type'] == "telemetry":
+                        await self.handle_telemetry(j)
+                    if j['type'] == "text":
+                        await self.handle_text(j)
+                    if j['type'] == "traceroute":
+                        if 'route' in j['payload']:
+                            route = []
+                            for r in j['payload']['route']:
+                                node = self.data.find_node_by_longname(r)
+                                if node is not None:
+                                    id = node['id']
+                                else:
+                                    id = None
+                                route.append(id)
+                            j['route'] = route
+                        await self.handle_traceroute(j)
+                    await self.prune_expired_nodes()
+                except Exception as e:
+                    print(e)
+                    traceback.print_exc()
 
     async def publish(self, client, topic, msg):
         result = await client.publish(topic, msg)
