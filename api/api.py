1f81f0273955e9e5214cfb023012c665a5ea9629
Kevin Elliott <kevin@welikeinc.com>
2024-09-03 13:06:45 -0700

    next: support dev

--- api/api.py
+++ api/api.py
@@ -186,6 +186,7 @@ class API:
             app.add_middleware(
                 CORSMiddleware,
                 allow_origins=allow_origins,
+                allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"]
             )
--- frontend/scripts/run-frontend.sh
+++ frontend/scripts/run-frontend.sh
@@ -1,2 +1,8 @@
+echo "NODE_ENV: " $NODE_ENV
 echo "API BASE URL: " $VITE_API_BASE_URL
-yarn build --base=/next/ && npx pm2-runtime /frontend/scripts/server.js
+
+if [ "$NODE_ENV" == "dev" ]; then
+  yarn run dev --host 0.0.0.0
+else
+  yarn build --base=/next/ && npx pm2-runtime /frontend/scripts/server.js
+fi
