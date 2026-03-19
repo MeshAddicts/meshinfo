816e60dd54b545820cd5448d2bc0da75ff448bae
SimmerV <simmer@nobscb.com>
2026-03-18 21:47:52 -0700

    feat: replace Express with Caddy in frontend container; fix lint warnings; update Yarn

--- frontend/scripts/run-frontend.sh
+++ frontend/scripts/run-frontend.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 echo "NODE_ENV: " $NODE_ENV
 echo "API BASE URL: " $VITE_API_BASE_URL
-if [ "$NODE_ENV" = "dev" ]; then
-  yarn run dev --host 0.0.0.0
-else
-  yarn build --base=/ && node /frontend/scripts/server.js
-fi
+yarn run dev --host 0.0.0.0
